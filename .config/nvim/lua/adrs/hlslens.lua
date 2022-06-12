7c7d0cab679ea95d4f41f017037120d85184331c
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-06-11 23:35:15 -0500

    [Feat] [Add lua files]

---
+++ .config/nvim/lua/adrs/hlslens.lua
@@ -0,0 +1,12 @@
+require("hlslens").setup({
+   build_position_cb = function(plist, _, _, _)
+        require("scrollbar.handlers.search").handler.show(plist.start_pos)
+   end,
+})
+
+vim.cmd([[
+    augroup scrollbar_search_hide
+        autocmd!
+        autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
+    augroup END
+]])
---
+++ .config/nvim/lua/adrs/init.lua
@@ -0,0 +1,2 @@
+require('adrs.hlslens')
+require('adrs.scrollbar')
