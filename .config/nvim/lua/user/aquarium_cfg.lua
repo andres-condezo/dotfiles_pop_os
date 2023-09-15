5f760b7deba10adcb72c479828f93e913094b65f
andres-condezo <ra.condezomonge@gmail.com>
2023-09-15 17:32:33 -0500

    Feat: Delete nvim directory

--- .config/nvim/lua/user/aquarium_cfg.lua
+++
@@ -1,4 +0,0 @@
-vim.cmd [[let g:aqua_transparency = 1]]
-vim.cmd [[let g:aquarium_style="dark"]]
-vim.cmd [[let g:aqua_bold = 1]]
-
--- .config/nvim/lua/user/everforest_cfg.lua
+++
@@ -1,2 +0,0 @@
-vim.g.everforest_background = "hard"
-vim.g.everforest_ui_contrast= "high"
